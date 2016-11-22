8d0da9bcf6810ae3f3c31875c228a394e060e563
Alexander Schmitz <arschmitz@gmail.com>
2016-11-22 13:29:46 -0500

    Repro select issue

--- tests/dummy/app/controllers/demo/select.js
+++ tests/dummy/app/controllers/demo/select.js
@@ -3,57 +3,44 @@ import Ember from 'ember';
 const { Controller, computed, RSVP, A, run } = Ember;
 
 export default Controller.extend({
-
-  userState: '',
   states: computed(function() {
     return A('AL AK AZ AR CA CO CT DE FL GA HI ID IL IN IA KS KY LA ME MD MA MI MN MS MO MT NE NV NH NJ NM NY NC ND OH OK OR PA RI SC SD TN TX UT VT VA WA WV WI WY'
             .split(' ').map((state) => ({ abbrev: state })));
   }),
 
-  sizes: A([
-    'small (12-inch)',
-    'medium (14-inch)',
-    'large (16-inch)',
-    'insane (42-inch)'
+  items: Ember.A([
+    { name: 'Afghanistan', code: 'AF' },
+    { name: 'Åland Islands', code: 'AX' },
+    { name: 'Albania', code: 'AL' },
+    { name: 'Algeria', code: 'DZ' },
+    { name: 'American Samoa', code: 'AS' },
+    { name: 'AndorrA', code: 'AD' },
+    { name: 'Angola', code: 'AO' },
+    { name: 'Anguilla', code: 'AI' },
+    { name: 'Antarctica', code: 'AQ' },
+    { name: 'Antigua and Barbuda', code: 'AG' }
   ]),
+  actions: {
+    updateFilter(str) {
+      this.set('searchText', str);
+    },
+    addCountry(name) {
+      this.get('items').addObject({ name, code: '' });
+    },
 
-  vegetables: A([
-    { name: 'Corn', checked: false },
-    { name: 'Onions', checked: false },
-    { name: 'Kale', checked: false },
-    { name: 'Arugula', checked: false },
-    { name: 'Peas', checked: false },
-    { name: 'Zucchini', checked: false }
-  ]),
-  /*
-   * Fake promise to fetch data, here you would use ember-data, jQuery.ajax or whatever you want.
-   */
-  users: computed(function() {
-    return new RSVP.Promise((resolve) => {
-      // Just wait for 800ms to 2 seconds for a fake progress, so it feels like a query.
-      let waitMS = Math.floor(Math.random() * 2000) + 800;
+    searchCountries(term) {
+      let XHR_TIMEOUT = Math.floor(Math.random() * 1000) + 100;
 
-      let dataFromServer = A([
-        { id: 1, name: 'Scooby Doo' },
-        { id: 2, name: 'Shaggy Rodgers' },
-        { id: 3, name: 'Fred Jones' },
-        { id: 4, name: 'Daphne Blake' },
-        { id: 5, name: 'Velma Dinkley' }
-      ]);
+      return new RSVP.Promise((resolve) => {
+        run.cancel(this.searchTimer);
 
-      run.later(() => {
-        /*
-         * Two arguments to the resolve:
-         * - data from the server
-         * - callback to be able to get the 'label'.
-         */
-        resolve(dataFromServer);
-      }, waitMS);
-
-    });
-  }),
-  groupedToppings: [
-    { groupName: 'Meats', options: ['Pepperoni', 'Sausage', 'Ground Beef', 'Bacon'] },
-    { groupName: 'Veg', options: ['Mushrooms', 'Onion', 'Green Pepper', 'Green Olives'] }
-  ]
+        this.searchTimer = run.later(this, () => {
+          let nameRegExp = new RegExp(escapeRegExp(`${term}`),'i', 'g');
+          let countries = this.get('items');
+          let results = countries.filter((c) => nameRegExp.exec(c.name)) || [];
+          resolve(results);
+        }, XHR_TIMEOUT);
+      });
+    }
+  }
 });
